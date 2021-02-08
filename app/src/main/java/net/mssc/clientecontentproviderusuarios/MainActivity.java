1801212bf942e5a3837307b200bdff90f768c316
Socorro Samano <48115672+mssc30@users.noreply.github.com>
2021-02-07 22:45:01 -0600

    Permisos WRITE y READ

--- app/src/main/java/net/mssc/clientecontentproviderusuarios/MainActivity.java
+++ app/src/main/java/net/mssc/clientecontentproviderusuarios/MainActivity.java
@@ -1,9 +1,11 @@
 package net.mssc.clientecontentproviderusuarios;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -22,22 +24,25 @@ public class MainActivity extends AppCompatActivity {
 
         ContentValues cv = new ContentValues();
 
-        cv.put(MiProviderContrato.Usuarios.NOMBRE, "Jhony");
+        Uri CONTENT_URI = MiProviderContrato.Usuarios.CONTENT_URI;
+
+        cv.put(MiProviderContrato.Usuarios.NOMBRE, "Koko");
         cv.put(MiProviderContrato.Usuarios.CONTRASENA, "123");
-        cv.put(MiProviderContrato.Usuarios.EMAIL, "jhony@google.com");
+        cv.put(MiProviderContrato.Usuarios.EMAIL, "kokoy@google.com");
         cv.put(MiProviderContrato.Usuarios.TELEFONO, "445");
 
-        Uri uri = cr.insert(MiProviderContrato.Usuarios.CONTENT_URI, cv);
+        Uri uri = cr.insert(CONTENT_URI, cv);
 
-        Cursor cursor = getContentResolver().query(
-                MiProviderContrato.Usuarios.CONTENT_URI,
+        Cursor c = cr.query(
+                CONTENT_URI,
                 null,
                 null,
                 null,
                 null);
 
-        while (cursor.moveToNext()){
-           Log.d("MCP", cursor.getInt(0) + " - " + cursor.getString(2));
+        while (c.moveToNext()) {
+            Log.d("MCP", c.getInt(1) + " - " + c.getString(2));
         }
+
     }
 }
